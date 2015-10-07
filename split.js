b5bdff436db9ce9fda4f289e8cb3d5c7070ed754
Nathan Cahill <nathan@nathancahill.com>
2015-10-06 20:26:45 -0600

    switch widths to use getBoundingClientRect

--- split.js
+++ split.js
@@ -19,8 +19,8 @@ Split = function (ids, options) {
 
             // Calculate the pairs width, and percentage of the parent width
 
-            this.width = this.left.clientWidth + this.right.clientWidth + options.gutterWidth;
-            this.percentage = Math.min(this.width / this.parent.clientWidth * 100, 100);
+            this.width = this.left.getBoundingClientRect().width + this.right.getBoundingClientRect().width + options.gutterWidth;
+            this.percentage = Math.min(this.width / this.parent.getBoundingClientRect().width * 100, 100);
 
             this.x = this.left.getBoundingClientRect().left;
 
