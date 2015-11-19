c86c9ea531c1493da920ac34ee3e160e236a1a75
Nathan Cahill <nathan@nathancahill.com>
2015-11-19 16:01:28 -0700

    update for eslint compatibility

--- split.js
+++ split.js
@@ -3,44 +3,30 @@
 
 (function() {
 
-var global = this,
-    querySelector = 'querySelector',
-    addEventListener = 'addEventListener',
-    removeEventListener = 'removeEventListener',
-    getBoundingClientRect = 'getBoundingClientRect';
+var global = this
+  , querySelector = 'querySelector'
+  , addEventListener = 'addEventListener'
+  , removeEventListener = 'removeEventListener'
+  , getBoundingClientRect = 'getBoundingClientRect'
+  , isIE8 = global.attachEvent && !global[addEventListener]
 
-// Array.isArray Polyfill
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+  , calc = (function () {
+        var el
+          , prefixes = ["", "-webkit-", "-moz-", "-o-"]
 
-if (!Array.isArray) {
-  Array.isArray = function(arg) {
-    return Object.prototype.toString.call(arg) === '[object Array]';
-  };
-}
+        for (var i = 0; i < prefixes.length; i++) {
+            el = global.document.createElement('div')
+            el.style.cssText = "width:" + prefixes[i] + "calc(9px)"
 
-// Check if browser is IE8 or lower
-
-var isIE8 = (function () {
-    return (global.attachEvent && !global[addEventListener]);
-})();
-
-// Get supported calc() prefix
-
-var calc = (function () {
-    var prefixes = ["", "-webkit-", "-moz-", "-o-"], el;
-
-    for (var i = 0; i < prefixes.length; i++) {
-        el = global.document.createElement('div');
-        el.style.cssText = "width:" + prefixes[i] + "calc(9px)";
-
-        if (el.style.length) {
-            return prefixes[i] + "calc";
+            if (el.style.length) {
+                return prefixes[i] + "calc"
+            }
         }
-    }
-})();
+    })()
 
-var Split = function (ids, options) {
+  , Split = function (ids, options) {
     var dimension
+      , i
       , clientDimension
       , clientAxis
       , position
@@ -51,30 +37,30 @@ var Split = function (ids, options) {
 
     // Set defaults
 
-    options = typeof options !== 'undefined' ?  options : {};
+    options = typeof options !== 'undefined' ?  options : {}
 
-    if (!options.gutterSize) options.gutterSize = 10;
-    if (!options.minSize) options.minSize = 100;
-    if (!options.snapOffset) options.snapOffset = 30;
-    if (!options.direction) options.direction = 'horizontal';
-    if (!options.cursor) options.cursor = 'grabbing';
+    if (!options.gutterSize) options.gutterSize = 10
+    if (!options.minSize) options.minSize = 100
+    if (!options.snapOffset) options.snapOffset = 30
+    if (!options.direction) options.direction = 'horizontal'
+    if (!options.cursor) options.cursor = 'grabbing'
 
     if (options.direction == 'horizontal') {
-        dimension = 'width';
-        clientDimension = 'clientWidth';
-        clientAxis = 'clientX';
-        position = 'left';
-        gutterClass = 'gutter gutter-horizontal';
-        paddingA = 'paddingLeft';
-        paddingB = 'paddingRight';
+        dimension = 'width'
+        clientDimension = 'clientWidth'
+        clientAxis = 'clientX'
+        position = 'left'
+        gutterClass = 'gutter gutter-horizontal'
+        paddingA = 'paddingLeft'
+        paddingB = 'paddingRight'
     } else if (options.direction == 'vertical') {
-        dimension = 'height';
-        clientDimension = 'clientHeight';
-        clientAxis = 'clientY';
-        position = 'top';
-        gutterClass = 'gutter gutter-vertical';
-        paddingA = 'paddingTop';
-        paddingB = 'paddingBottom';
+        dimension = 'height'
+        clientDimension = 'clientHeight'
+        clientAxis = 'clientY'
+        position = 'top'
+        gutterClass = 'gutter gutter-vertical'
+        paddingA = 'paddingTop'
+        paddingB = 'paddingBottom'
     }
 
     // Event listeners for drag events, bound to a pair object.
@@ -83,170 +69,163 @@ var Split = function (ids, options) {
 
     var startDragging = function (e) {
             if (!this.dragging && options.onDragStart) {
-                options.onDragStart();
+                options.onDragStart()
             }
 
-            e.preventDefault();
+            e.preventDefault()
 
-            this.dragging = true;
+            this.dragging = true
 
-            this.a[addEventListener]('selectstart', preventSelection);
-            this.a[addEventListener]('dragstart', preventSelection);
-            this.b[addEventListener]('selectstart', preventSelection);
-            this.b[addEventListener]('dragstart', preventSelection);
+            this.a[addEventListener]('selectstart', preventSelection)
+            this.a[addEventListener]('dragstart', preventSelection)
+            this.b[addEventListener]('selectstart', preventSelection)
+            this.b[addEventListener]('dragstart', preventSelection)
 
-            this.a.style.userSelect = 'none';
-            this.a.style.webkitUserSelect = 'none';
-            this.a.style.MozUserSelect = 'none';
-            this.a.style.pointerEvents = 'none';
-            this.a.style.cursor = options.cursor;
+            this.a.style.userSelect = 'none'
+            this.a.style.webkitUserSelect = 'none'
+            this.a.style.MozUserSelect = 'none'
+            this.a.style.pointerEvents = 'none'
+            this.a.style.cursor = options.cursor
 
-            this.b.style.userSelect = 'none';
-            this.b.style.webkitUserSelect = 'none';
-            this.b.style.MozUserSelect = 'none';
-            this.b.style.pointerEvents = 'none';
-            this.b.style.cursor = options.cursor;
+            this.b.style.userSelect = 'none'
+            this.b.style.webkitUserSelect = 'none'
+            this.b.style.MozUserSelect = 'none'
+            this.b.style.pointerEvents = 'none'
+            this.b.style.cursor = options.cursor
 
-            this.gutter.style.cursor = options.cursor;
+            this.gutter.style.cursor = options.cursor
 
-            calculateSizes.call(this);
-        },
-
-        stopDragging = function () {
+            calculateSizes.call(this)
+        }
+      , stopDragging = function () {
             if (this.dragging && options.onDragEnd) {
-                options.onDragEnd();
+                options.onDragEnd()
             }
 
-            this.dragging = false;
+            this.dragging = false
 
-            this.a[removeEventListener]('selectstart', preventSelection);
-            this.a[removeEventListener]('dragstart', preventSelection);
-            this.b[removeEventListener]('selectstart', preventSelection);
-            this.b[removeEventListener]('dragstart', preventSelection);
+            this.a[removeEventListener]('selectstart', preventSelection)
+            this.a[removeEventListener]('dragstart', preventSelection)
+            this.b[removeEventListener]('selectstart', preventSelection)
+            this.b[removeEventListener]('dragstart', preventSelection)
 
-            this.a.style.userSelect = '';
-            this.a.style.webkitUserSelect = '';
-            this.a.style.MozUserSelect = '';
-            this.a.style.pointerEvents = '';
-            this.a.style.cursor = '';
+            this.a.style.userSelect = ''
+            this.a.style.webkitUserSelect = ''
+            this.a.style.MozUserSelect = ''
+            this.a.style.pointerEvents = ''
+            this.a.style.cursor = ''
 
-            this.b.style.userSelect = '';
-            this.b.style.webkitUserSelect = '';
-            this.b.style.MozUserSelect = '';
-            this.b.style.pointerEvents = '';
-            this.b.style.cursor = '';
+            this.b.style.userSelect = ''
+            this.b.style.webkitUserSelect = ''
+            this.b.style.MozUserSelect = ''
+            this.b.style.pointerEvents = ''
+            this.b.style.cursor = ''
 
-            this.gutter.style.cursor = '';
-        },
+            this.gutter.style.cursor = ''
+        }
+      , drag = function (e) {
+            var offset
 
-        drag = function (e) {
-            if (!this.dragging) return;
+            if (!this.dragging) return
 
             // Get the relative position of the event from the first side of the
             // pair.
 
             if ('touches' in e) {
-                var offset = e.touches[0][clientAxis] - this.start;
+                offset = e.touches[0][clientAxis] - this.start
             } else {
-                var offset = e[clientAxis] - this.start;
+                offset = e[clientAxis] - this.start
             }
 
             // If within snapOffset of min or max, set offset to min or max
 
             if (offset <=  this.aMin + options.snapOffset) {
-                offset = this.aMin;
+                offset = this.aMin
             } else if (offset >= this.size - this.bMin - options.snapOffset) {
-                offset = this.size - this.bMin;
+                offset = this.size - this.bMin
             }
 
-            adjust.call(this, offset);
+            adjust.call(this, offset)
 
             if (options.onDrag) {
-                options.onDrag();
+                options.onDrag()
             }
-        },
-
-        calculateSizes = function () {
+        }
+      , calculateSizes = function () {
             // Calculate the pairs size, and percentage of the parent size
             var computedStyle = global.getComputedStyle(this.parent)
               , parentSize = this.parent[clientDimension] - parseFloat(computedStyle[paddingA]) - parseFloat(computedStyle[paddingB])
 
-            this.size = this.a[getBoundingClientRect]()[dimension] + this.b[getBoundingClientRect]()[dimension] + this.aGutterSize + this.bGutterSize;
-            this.percentage = Math.min(this.size / parentSize * 100, 100);
-            this.start = this.a[getBoundingClientRect]()[position];
-        },
-
-        adjust = function (offset) {
+            this.size = this.a[getBoundingClientRect]()[dimension] + this.b[getBoundingClientRect]()[dimension] + this.aGutterSize + this.bGutterSize
+            this.percentage = Math.min(this.size / parentSize * 100, 100)
+            this.start = this.a[getBoundingClientRect]()[position]
+        }
+      , adjust = function (offset) {
             // A size is the same as offset. B size is total size - A size.
             // Both sizes are calculated from the initial parent percentage.
 
-            this.a.style[dimension] = calc + '(' + (offset / this.size * this.percentage) + '% - ' + this.aGutterSize + 'px)';
-            this.b.style[dimension] = calc + '(' + (this.percentage - (offset / this.size * this.percentage)) + '% - ' + this.bGutterSize + 'px)';
-        },
-
-        fitMin = function () {
+            this.a.style[dimension] = calc + '(' + (offset / this.size * this.percentage) + '% - ' + this.aGutterSize + 'px)'
+            this.b.style[dimension] = calc + '(' + (this.percentage - (offset / this.size * this.percentage)) + '% - ' + this.bGutterSize + 'px)'
+        }
+      , fitMin = function () {
             if (this.a[getBoundingClientRect]()[dimension] < this.aMin) {
-                this.a.style[dimension] = (this.aMin - this.aGutterSize) + 'px';
-                this.b.style[dimension] = (this.size - this.aMin - this.aGutterSize) + 'px';
+                this.a.style[dimension] = (this.aMin - this.aGutterSize) + 'px'
+                this.b.style[dimension] = (this.size - this.aMin - this.aGutterSize) + 'px'
             } else if (this.b[getBoundingClientRect]()[dimension] < this.bMin) {
-                this.a.style[dimension] = (this.size - this.bMin - this.bGutterSize) + 'px';
-                this.b.style[dimension] = (this.bMin - this.bGutterSize) + 'px';
+                this.a.style[dimension] = (this.size - this.bMin - this.bGutterSize) + 'px'
+                this.b.style[dimension] = (this.bMin - this.bGutterSize) + 'px'
             }
-        },
-
-        fitMinReverse = function () {
+        }
+      , fitMinReverse = function () {
             if (this.b[getBoundingClientRect]()[dimension] < this.bMin) {
-                this.a.style[dimension] = (this.size - this.bMin - this.bGutterSize) + 'px';
-                this.b.style[dimension] = (this.bMin - this.bGutterSize) + 'px';
+                this.a.style[dimension] = (this.size - this.bMin - this.bGutterSize) + 'px'
+                this.b.style[dimension] = (this.bMin - this.bGutterSize) + 'px'
             } else if (this.a[getBoundingClientRect]()[dimension] < this.aMin) {
-                this.a.style[dimension] = (this.aMin - this.aGutterSize) + 'px';
-                this.b.style[dimension] = (this.size - this.aMin - this.aGutterSize) + 'px';
+                this.a.style[dimension] = (this.aMin - this.aGutterSize) + 'px'
+                this.b.style[dimension] = (this.size - this.aMin - this.aGutterSize) + 'px'
             }
-        },
-
-        balancePairs = function (pairs) {
+        }
+      , balancePairs = function (pairs) {
             for (var i = 0; i < pairs.length; i++) {
-                calculateSizes.call(pairs[i]);
-                fitMin.call(pairs[i]);
+                calculateSizes.call(pairs[i])
+                fitMin.call(pairs[i])
             }
 
             for (i = pairs.length - 1; i >= 0; i--) {
-                calculateSizes.call(pairs[i]);
-                fitMinReverse.call(pairs[i]);
+                calculateSizes.call(pairs[i])
+                fitMinReverse.call(pairs[i])
             }
-        },
-
-        preventSelection = function () { return false; },
-
-        parent = global.document[querySelector](ids[0]).parentNode;
+        }
+      , preventSelection = function () { return false }
+      , parent = global.document[querySelector](ids[0]).parentNode
 
     if (!options.sizes) {
-        var percent = 100 / ids.length;
+        var percent = 100 / ids.length
 
-        options.sizes = [];
+        options.sizes = []
 
-        for (var i = 0; i < ids.length; i++) {
-            options.sizes.push(percent);
+        for (i = 0; i < ids.length; i++) {
+            options.sizes.push(percent)
         }
     }
 
     if (!Array.isArray(options.minSize)) {
-        var minSizes = [];
+        var minSizes = []
 
-        for (var i = 0; i < ids.length; i++) {
-            minSizes.push(options.minSize);
+        for (i = 0; i < ids.length; i++) {
+            minSizes.push(options.minSize)
         }
 
-        options.minSize = minSizes;
+        options.minSize = minSizes
     }
 
-    for (var i = 0; i < ids.length; i++) {
-        var el = global.document[querySelector](ids[i]),
-            isFirst = (i == 1),
-            isLast = (i == ids.length - 1),
-            size,
-            gutterSize = options.gutterSize,
-            pair;
+    for (i = 0; i < ids.length; i++) {
+        var el = global.document[querySelector](ids[i])
+          , isFirst = (i == 1)
+          , isLast = (i == ids.length - 1)
+          , size
+          , gutterSize = options.gutterSize
+          , pair
 
         if (i > 0) {
             pair = {
@@ -259,81 +238,81 @@ var Split = function (ids, options) {
                 isFirst: isFirst,
                 isLast: isLast,
                 direction: options.direction
-            };
+            }
 
             // For first and last pairs, first and last gutter width is half.
 
-            pair.aGutterSize = options.gutterSize;
-            pair.bGutterSize = options.gutterSize;
+            pair.aGutterSize = options.gutterSize
+            pair.bGutterSize = options.gutterSize
 
             if (isFirst) {
-                pair.aGutterSize = options.gutterSize / 2;
+                pair.aGutterSize = options.gutterSize / 2
             }
 
             if (isLast) {
-                pair.bGutterSize = options.gutterSize / 2;
+                pair.bGutterSize = options.gutterSize / 2
             }
         }
 
         // IE9 and above
         if (!isIE8) {
             if (i > 0) {
-                var gutter = global.document.createElement('div');
+                var gutter = global.document.createElement('div')
 
-                gutter.className = gutterClass;
-                gutter.style[dimension] = options.gutterSize + 'px';
+                gutter.className = gutterClass
+                gutter.style[dimension] = options.gutterSize + 'px'
 
-                gutter[addEventListener]('mousedown', startDragging.bind(pair));
-                parent[addEventListener]('mouseup', stopDragging.bind(pair));
-                parent[addEventListener]('mousemove', drag.bind(pair));
-                parent[addEventListener]('mouseleave', stopDragging.bind(pair));
+                gutter[addEventListener]('mousedown', startDragging.bind(pair))
+                parent[addEventListener]('mouseup', stopDragging.bind(pair))
+                parent[addEventListener]('mousemove', drag.bind(pair))
+                parent[addEventListener]('mouseleave', stopDragging.bind(pair))
 
-                gutter[addEventListener]('touchstart', startDragging.bind(pair));
-                parent[addEventListener]('touchend', stopDragging.bind(pair));
-                parent[addEventListener]('touchcancel', stopDragging.bind(pair));
-                parent[addEventListener]('touchmove', drag.bind(pair));
+                gutter[addEventListener]('touchstart', startDragging.bind(pair))
+                parent[addEventListener]('touchend', stopDragging.bind(pair))
+                parent[addEventListener]('touchcancel', stopDragging.bind(pair))
+                parent[addEventListener]('touchmove', drag.bind(pair))
 
-                parent.insertBefore(gutter, el);
+                parent.insertBefore(gutter, el)
 
-                pair.gutter = gutter;
+                pair.gutter = gutter
             }
 
             if (i === 0 || i == ids.length - 1) {
-                gutterSize = options.gutterSize / 2;
+                gutterSize = options.gutterSize / 2
             }
 
             if (typeof options.sizes[i] === 'string' || options.sizes[i] instanceof String) {
-                size = options.sizes[i];
+                size = options.sizes[i]
             } else {
-                size = calc + '(' + options.sizes[i] + '% - ' + gutterSize + 'px)';
+                size = calc + '(' + options.sizes[i] + '% - ' + gutterSize + 'px)'
             }
 
         // IE8 and below
         } else {
             if (typeof options.sizes[i] === 'string' || options.sizes[i] instanceof String) {
-                size = options.sizes[i];
+                size = options.sizes[i]
             } else {
-                size = options.sizes[i] + '%';
+                size = options.sizes[i] + '%'
             }
         }
 
-        el.style[dimension] = size;
+        el.style[dimension] = size
 
         if (i > 0) {
-            pairs.push(pair);
+            pairs.push(pair)
         }
     }
 
-    balancePairs(pairs);
-};
+    balancePairs(pairs)
+}
 
 if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
-        exports = module.exports = Split;
+        exports = module.exports = Split
     }
-    exports.Split = Split;
+    exports.Split = Split
 } else {
-    global.Split = Split;
+    global.Split = Split
 }
 
-}).call(window);
+}).call(window)
