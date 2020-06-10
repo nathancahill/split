5de27ae16547a48273d06b346025d8715aaa151a
Nathan Cahill <nathan@nathancahill.com>
2020-06-10 14:04:08 +0200

    version bump and format

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    trailingComma: "all",
+    trailingComma: 'all',
     tabWidth: 4,
     semi: false,
-    singleQuote: true
-};
+    singleQuote: true,
+    arrowParens: 'avoid',
+}
