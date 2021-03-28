ba78b9133f21d3c9626d0ca004abae9381f2dc54
Nathan Cahill <nathan@nathancahill.com>
2021-03-28 19:19:33 +0200

    add link to docs

--- packages/split-generator/src/Dashboard.svelte
+++ packages/split-generator/src/Dashboard.svelte
@@ -101,7 +101,8 @@
                 class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left"
             >
                 <span class="block sm:inline"
-                    >&copy; {(new Date()).getFullYear()} Nathan Cahill</span
+                    >&copy; {(new Date()).getFullYear()}
+                    <a href="https://nathancahill.com/">Nathan Cahill</a></span
                 >
             </div>
         </div>
